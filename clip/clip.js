dc82d00694ba1377530751372464e52a32c87631
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2022-04-15 16:15:12 +0300

    fix: initial parameters

--- clip/clip.js
+++ clip/clip.js
@@ -24,7 +24,7 @@ import {
   rZb01,
 } from "./incidents";
 const initParamsValue = initParams[0].value;
-const rows = Math.sqrt(initParamsValue.totalBoxes);
+const rows = initParamsValue.sideLength;
 const columns = rows;
 
 export const clip = new HTMLClip({
@@ -185,3 +185,4 @@ clipMotion.addIncident(
 
 clipMotion.addIncident(randomIn, 0);
 clip.addIncident(clipMotion, 0);
+
--- clip/initParams.js
+++ clip/initParams.js
@@ -1,11 +1,11 @@
-const count = 14;
 export default [
   {
     name: "Default",
     value: {
-      width: 100 / count + "%",
-      height: 100 / count + "%",
-      totalBoxes: count * count,
+      sideLength: 12,
+      color: "#fff",
+      backgroundColor: "#121212",
     },
   },
 ];
+
